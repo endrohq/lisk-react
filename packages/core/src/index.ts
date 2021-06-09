16451a130759b42ea143a68b1997c2b97f2fa939
Tom Ploem <tom.ploem@gmail.com>
2021-06-09 09:08:39 +0200

    removal of nested useWallet

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,4 @@
 export { Api } from "./api/index";
-export { useWallet } from "./hooks/useWallet";
 export { useNetwork } from "./hooks/useNetwork";
 export { useClient } from "./hooks/useClient";
 export { setupWsClient } from "./client";
--- packages/use-wallet/src/index.tsx
+++ packages/use-wallet/src/index.tsx
@@ -1,6 +1,8 @@
 import React, { FC, useContext, useEffect, useMemo, useState } from "react";
-import { NetworkEndpoint, Wallet } from "@lisk-react/types";
-import { useWallet, useClient } from "@lisk-react/core";
+import { LiskAccount, NetworkEndpoint, Wallet } from "@lisk-react/types";
+import { useClient, useNetwork } from "@lisk-react/core";
+import { getAccountByPassphrase } from "@lisk-react/core/dist/utils/account.utils";
+import accountFactory from "@lisk-react/core/dist/factory/account.factory";
 
 export interface LiskWalletContextStateProps extends Wallet {
   setEndpoint(endpoint?: NetworkEndpoint): void;
@@ -21,18 +23,63 @@ export const LiskWalletProvider: FC<Props> = ({ endpoint, children }) => {
   const [networkEndpoint, setNetworkEndpoint] = useState<NetworkEndpoint>();
 
   const { client } = useClient({ endpoint: networkEndpoint });
-  const wallet = useWallet({ client, endpoint: networkEndpoint });
+  const { accounts } = useNetwork({ client, endpoint });
+
+  const [account, setAccount] = useState<LiskAccount>();
+  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
     if (endpoint?.wsUrl) setNetworkEndpoint(endpoint);
   }, [endpoint]);
 
+  useEffect(() => {
+    const updatedAccount = accounts.find(
+      (item) => item?.address === account?.address
+    );
+    if (updatedAccount && Object.keys(updatedAccount)?.length > 0) {
+      setAccount(updatedAccount);
+    }
+  }, [accounts]);
+
+  async function authenticate(passphrase: string): Promise<void> {
+    const account = getAccountByPassphrase(passphrase);
+    if (!account) return;
+    if (client && account?.address) {
+      updateAccount(account?.address);
+    } else {
+      setAccount(account);
+    }
+  }
+
+  function logout() {
+    setAccount(undefined);
+  }
+
+  async function updateAccount(address: string): Promise<void> {
+    await setLoading(true);
+    try {
+      if (client) {
+        const account = (await client.account.get(address)) as LiskAccount;
+        setAccount(account);
+      }
+    } catch (error) {
+    } finally {
+      setLoading(false);
+    }
+  }
+
   const value = useMemo(
     () => ({
-      ...wallet,
+      isAuthenticated: !!account,
+      authenticate,
+      logout,
+      account,
+      generate: () => accountFactory.create(),
+      setAccount: (account: LiskAccount) => setAccount(account),
+      loading,
       setEndpoint: (endpoint: NetworkEndpoint) => setNetworkEndpoint(endpoint),
     }),
-    [endpoint]
+    [endpoint, loading, account]
   );
 
   return (
